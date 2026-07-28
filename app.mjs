4ecc0c0652c664cebd6e5c7c06de81208e99017d
ri-kawashima@ktec-dev.com <244881180+ri-kawashima_ktec@users.noreply.github.com>
2026-07-28 12:50:55 +0900

    SHT40に変更（実センサーがSHT4xだったため）

--- app.mjs
+++ app.mjs
@@ -1,14 +1,14 @@
-import { requestI2CAccess, SHT30 } from "chirimen";
+import { requestI2CAccess, SHT40 } from "chirimen";
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const i2cAccess = await requestI2CAccess();
 const port = i2cAccess.ports.get(1);
-const sht30 = new SHT30(port, 0x44);
-await sht30.init();
+const sht40 = new SHT40(port, 0x44);
+await sht40.init();
 
 while (true) {
-  const { humidity, temperature } = await sht30.readData();
+  const { humidity, temperature } = await sht40.readData();
   console.log("Humidity: " + humidity.toFixed(2) + "%,Temperature: " + temperature.toFixed(2) + "℃");
   await sleep(500);
 }
