8de4dd6f0403e3d1ccb749b7b2fa14e6f0fad8d5
ri-kawashima@ktec-dev.com <244881180+ri-kawashima_ktec@users.noreply.github.com>
2026-07-28 10:27:21 +0900

    SHT30のコード追加

--- app.mjs
+++ app.mjs
@@ -1,3 +1,14 @@
-import {requestI2CAccess} from "chirimen";
+import { requestI2CAccess, SHT30 } from "chirimen";
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const i2cAccess = await requestI2CAccess();
+const port = i2cAccess.ports.get(1);
+const sht30 = new SHT30(port, 0x44);
+await sht30.init();
+
+while (true) {
+  const { humidity, temperature } = await sht30.readData();
+  console.log("Humidity: " + humidity.toFixed(2) + "%,Temperature: " + temperature.toFixed(2) + "℃");
+  await sleep(500);
+}
